3aeb94f4757ee19aae87976516387b72fa465602
Dsh1124 <126290369+Dsh1124@users.noreply.github.com>
2023-02-26 23:19:01 +0300

    Add files via upload

---
+++ triangle.h
@@ -0,0 +1,10 @@
+class Triangle {
+    private:
+        double a, b, c;
+    public:
+        bool exst_tr();
+        void Set(double a1, double b1, double c1);
+        void show();
+        double perimetr();
+        double square();
+};
