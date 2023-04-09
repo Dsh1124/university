f1190a0f3175a969f505a0a6dcf1ea7179803783
Dsh1124 <126290369+Dsh1124@users.noreply.github.com>
2023-04-09 21:05:46 +0300

    Add files via upload

---
+++ type3.cpp
@@ -0,0 +1,10 @@
+#include "type3.h";
+#include <iostream>
+#include <cmath>
+using namespace std;
+void type3 :: Get_answer () {
+    cout << "Корней нет" << endl;
+}
+void type3 :: show () {
+    cout << C << " = 0"<< endl;
+}  
